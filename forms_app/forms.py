bd03745f747feda7d8ebc8907d0096dfca4d4f2f
Pelane Sanon <p_sanon@aol.com>
2018-10-16 16:11:48 -0500

    Doone

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -5,6 +5,6 @@ from .models import FormModel
 class PostForm(forms.ModelForm):
     class Meta:
         model = FormModel
-        fields = {'name', 'address', 'operating_budget', 'number_of_employees'}
-        order_field = ['name', 'address', 'operating_budget', 'number_of_employees']
+        fields = {'name', 'address', 'operating_budget', 'description', 'number_of_employees', 'image_url'}
+        order_field = ['name', 'address', 'operating_budget', 'description', 'number_of_employees']
 
--- forms_app/models.py
+++ forms_app/models.py
@@ -6,8 +6,10 @@ class FormModel(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=500)
     operating_budget = models.IntegerField(default=0)
+    description = models.TextField(null=True, max_length=500)
     number_of_employees = models.IntegerField(default=0)
     established_date = models.DateTimeField(default=datetime.now)
+    image_url = models.URLField(null=True, max_length=500)
 
     def __str__(self):
         return self.name
